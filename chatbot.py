9462f004af07b2f76af29308ce5a782a81506277
carol-loves-cats <1429531820@qq.com>
2024-03-06 15:49:45 +0800

    lab6

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
-import configparser
+#import configparser
+import os
 import logging
 import redis
 
@@ -15,14 +16,18 @@ def equiped_chatgpt(update, context):
 global redis1
 def main():
   # Load your token and create an Updater for your Bot
-  config = configparser.ConfigParser()
-  config.read('config.ini')
-  updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+  #config = configparser.ConfigParser()
+  #config.read('config.ini')
+  #updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+  updater = Updater(token=(os.environ['ACCESS_TOKEN'], use_context=True)
   dispatcher = updater.dispatcher
   global redis1
-  redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-  password=(config['REDIS']['PASSWORD']),
-  port=(config['REDIS']['REDISPORT']))
+  #redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+  #password=(config['REDIS']['PASSWORD']),
+  #port=(config['REDIS']['REDISPORT']))
+  redis1 = redis.Redis(host=(os.environ['REDIS_HOST']),
+  password=(os.environ['REDIS_PASSWORD']),
+  port=(os.environ['REDIS_REDISPORT']))
   # You can set this logging module, so you will know when
   # and why things do not work as expected Meanwhile, update your config.ini as:
   logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
