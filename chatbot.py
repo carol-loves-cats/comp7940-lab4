e2599f6636e4c0b69a09723dca0d3a3711dc3387
carol-loves-cats <1429531820@qq.com>
2024-03-06 15:57:31 +0800

    lab6

--- chatbot.py
+++ chatbot.py
@@ -38,7 +38,7 @@ def main():
   # dispatcher.add_handler(echo_handler)
   # dispatcher for chatgpt
   global chatgpt
-  chatgpt = HKBU_ChatGPT(config)
+  chatgpt = HKBU_ChatGPT()
   chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
   dispatcher.add_handler(chatgpt_handler)
 
